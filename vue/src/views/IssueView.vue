ae45a386eb950b4c52948bab3d2d071c24074945
Suvichada Pinittanond <suvipinittanond@gmail.com>
2024-06-05 15:58:53 -0400

    IssueView work PT. 2

--- vue/src/views/IssueView.vue
+++ vue/src/views/IssueView.vue
@@ -1,7 +1,16 @@
 <template>
 <div class='issues'>
   <h1>Issues</h1>
-  <p>{{ issues }}</p>
+  <ul class='issue-list'>
+      <li v-for="issue in issues" :key="issue.id">
+        <div class="issue-item">
+          <h3>{{ issue.name }}</h3>
+          <p>{{ issue.description }}</p>
+          <p>{{ issue.option1 }}</p>
+          <p>{{ issue.option2 }}</p>
+        </div>
+      </li>
+    </ul>
 </div>
 </template>
 
@@ -15,7 +24,7 @@ export default {
     }
   },
   created() {
-    IssueService.getIssues
+    IssueService.getIssues()
     .then(
       (response) => {
         this.issues = response.data;
@@ -25,5 +34,23 @@ export default {
 </script>
 
 <style>
+.issue-list {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  list-style: none;
+  padding: 0;
+}
 
+.issue-item {
+  
+  background-color: #c9c9c6;
+  height: 400px;
+  width: 350px;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  text-align: center; 
+  
+}
 </style>
